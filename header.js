9b0e1cf7e2fc68d7cf6d4266f3955c2a7214e6d1
Coleman DuPlessie <coleyduplessie@gmail.com>
2024-09-27 20:38:37 -0400

    Added a second page

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-document.write(`<body><h1 id="header">Coleman DuPlessie's High School Senior Project <a href="./index.html">Home</a></h1></body>`);
+document.write(`<h1 id="header">Coleman DuPlessie's High School Senior Project <a href="./index.html">Home</a><a href="./jobyouwant.html">Style</a></h1>`);
